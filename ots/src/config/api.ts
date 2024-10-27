c0007a20731e87297fac46c836168fe78287ddab
Aldo Zulhaq <imahumanty@gmail.com>
2024-10-27 19:08:10 +0700

    Update api.ts

--- ots/src/config/api.ts
+++ ots/src/config/api.ts
@@ -1,4 +1,4 @@
-const PROD_API_URL = import.meta.env.VITE_API_BASE_URL;
+const PROD_API_URL = import.meta.env.VITE_API_BASE_URL_PROD
 const DEV_API_URL = import.meta.env.VITE_API_BASE_URL_DEV
 
 export const API_BASE_URL = import.meta.env.DEV ? DEV_API_URL : PROD_API_URL;
